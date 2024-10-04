f0b0e8ca06247f7262f18d00357799ee6687a229
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-10-04 18:20:45 +0600

    Problem - 2
    Problem - 2

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 for (let i = 1; i <= 10; i++) {
   console.log("i = ", i);
 }
+
+// Loops Problem-2 (Sum of 1 to n numbers)
+let sum = 0;
+let n = prompt("Enter the final number to sum: ");
+
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
